a4f615b4b0c96ed5ad07ec58c80de6d6a66bed71
Chris Langsford <langz020@gmail.com>
2017-12-01 21:06:08 +0200

    Added ArrayList method to reader

--- src/main/java/fileReader/FileReader.java
+++ src/main/java/fileReader/FileReader.java
@@ -2,6 +2,8 @@ package fileReader;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -13,6 +15,11 @@ public class FileReader {
     public FileReader(String inputFileLocation) {
         this.inputFileLocation = inputFileLocation;
         this.inputFile = initFile();
+    }
+
+    private File initFile(){
+        ClassLoader classLoader = getClass().getClassLoader();
+        return new File(classLoader.getResource(inputFileLocation).getFile());
     }
 
     public String readFileIntoString(){
@@ -34,8 +41,28 @@ public class FileReader {
         return result.toString();
     }
 
-    private File initFile(){
-        ClassLoader classLoader = getClass().getClassLoader();
-        return new File(classLoader.getResource(inputFileLocation).getFile());
+    public List<Integer> readFileIntoIntegerArrayList(){
+        List<Integer> result = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(inputFile)) {
+            scanner.useDelimiter("");
+            while (scanner.hasNextInt()) {
+                int num = scanner.nextInt();
+                result.add(num);
+            }
+
+            scanner.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+
     }
+
+
+
+
+
 }
