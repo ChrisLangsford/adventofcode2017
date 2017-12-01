f0411b93c732c03ac3bf36c9182da8c48764851c
Chris Langsford <langz020@gmail.com>
2017-12-01 21:04:44 +0200

    Added logic to solve day 1

--- src/main/java/day1/CaptchaSolver.java
+++ src/main/java/day1/CaptchaSolver.java
@@ -3,11 +3,34 @@ package day1;
 
 import fileReader.FileReader;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class CaptchaSolver {
 
-    public static void main( String args[]){
-        String input  = new FileReader("input/day1.txt").readFileIntoString();
-        System.out.println(input);
+    public static void main(String args[]){
+        List<Integer> inputNumber = new FileReader("input/day1.txt").readFileIntoIntegerArrayList();
+        int sum = 0;
+        //int jump = 1; // Part 1
+        int jump = inputNumber.size()/2; // Part 2
 
+        for(int i=0; i < inputNumber.size(); i++){
+            if(inputNumber.get(i).equals(inputNumber.get(wrapIndex(inputNumber.size(), i+jump)))){
+                sum+=inputNumber.get(i);
+                System.out.println("size: "+ inputNumber.size());
+            }
+
+        }
+        System.out.println(sum);
+
+    }
+
+    private static int wrapIndex(int maxSize, int currentIndex){
+        if (currentIndex >= maxSize){
+            return currentIndex-maxSize;
+        } else {
+            return currentIndex;
+        }
     }
 }
