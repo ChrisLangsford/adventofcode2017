7e4ddabbfebe101092ed9cf97a2239f5b2a8d060
Chris Langsford <langz020@gmail.com>
2017-12-01 16:42:29 +0200

    Added first input and File Reader module

---
+++ src/main/java/fileReader/FileReader.java
@@ -0,0 +1,41 @@
+package fileReader;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class FileReader {
+
+    private String inputFileLocation;
+    private File inputFile;
+
+    public FileReader(String inputFileLocation) {
+        this.inputFileLocation = inputFileLocation;
+        this.inputFile = initFile();
+    }
+
+    public String readFileIntoString(){
+        StringBuilder result = new StringBuilder("");
+
+        try (Scanner scanner = new Scanner(inputFile)) {
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                result.append(line).append("\n");
+            }
+
+            scanner.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result.toString();
+    }
+
+    private File initFile(){
+        ClassLoader classLoader = getClass().getClassLoader();
+        return new File(classLoader.getResource(inputFileLocation).getFile());
+    }
+}
