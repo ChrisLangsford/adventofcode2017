e0b1d25c602ca5be3bd6aba7abe43e9ca2f41493
Chris Langsford <langz020@gmail.com>
2017-12-02 09:04:59 +0200

    Added Part 2

--- src/main/java/day2/CheckSumCalculator.java
+++ src/main/java/day2/CheckSumCalculator.java
@@ -3,18 +3,27 @@ package day2;
 import fileReader.FileReader;
 
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 
 public class CheckSumCalculator {
     public static void main(String[] args){
-        int checksum = 0;
+        int part1 = 0;
+        int part2 = 0;
         List<List<Integer>> inputList = new FileReader("input/day2.txt").readfileIntoListofIntArrays();
 
         for(List<Integer> row : inputList){
-            checksum+= Collections.max(row) - Collections.min(row);
+            part1 += Collections.max(row) - Collections.min(row);
+
+            for(int i=0; i< row.size(); i++){
+                for(int j=0; j < row.size(); j++){
+                    if(i !=j && row.get(i)%row.get(j)==0){
+                        part2+=row.get(i)/row.get(j);
+                    }
+                }
+            }
         }
 
-        System.out.println(checksum);
+        System.out.println("Part 1: "+ part1);
+        System.out.println("Part 2: "+ part2);
     }
 }
