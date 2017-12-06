361b5315da1dd71068da01cb73e6e5ed8bcdf3d5
Chris Langsford <langz020@gmail.com>
2017-12-06 15:49:08 +0200

    Day 6 reading input working

---
+++ src/main/java/day6/MemoryReallocationCounter.java
@@ -0,0 +1,13 @@
+package day6;
+
+import fileReader.FileReader;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class MemoryReallocationCounter {
+    public static void main(String[] args){
+        List<Integer> input = new FileReader("input/day6.txt").readFileIntoIntegerArrayList("\t");
+
+    }
+}
